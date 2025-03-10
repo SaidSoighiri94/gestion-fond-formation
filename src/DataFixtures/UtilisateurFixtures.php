5ecbc6c3e633046176acbd340d56a1189d47633e
SaidSoighiri94 <msaidsoighir@gmail.com>
2025-03-10 01:14:22 +0100

    Ajout des fixtures de l'entity utilisateur

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class UtilisateurFixtures extends Fixture
 {
@@ -11,6 +13,21 @@ class UtilisateurFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        // Faker's instance creation
+        $faker = Factory::create();
+
+        // Creation of 15 users
+
+        for($i =0; $i<=20; $i++){
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNomUtilisateur($faker->lastName);
+            $utilisateur->setEmailUtilisateur($faker->email);
+            $utilisateur->setPassword($faker->password);
+            $utilisateur->setTelUtilisateur($faker->phoneNumber)
+
+
+        }
 
         $manager->flush();
     }
